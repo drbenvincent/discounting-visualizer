4c075547267837e521a082f5de439331b0f8ca14
Ben Vincent <b.t.vincent@dundee.ac.uk>
2017-02-09 12:27:52 +0000

    upload of files

---
+++ README.md
@@ -0,0 +1,14 @@
+# discounting-visualizer
+
+Made by Benjamin T. Vincent of [inferenceLab.com](https://www.inferencelab.com).
+
+If you like this, then spread the word.
+___
+
+Here is a little interactive R / Shiny app I made to help visualize discount functions.
+
+To run it, download the code and load up `app_hyperbolic/app.R` in RStudio, then click the "run app" Button. You should then see something like this.
+
+![alt text](pics/example-hyperbolic.png "Logo Title Text 1")
+
+Note that you can drag and drop the graphs (onto your desktop for example) in order to include them in any of your own documents.
---
+++ app_hyperbolic/app.R
@@ -0,0 +1,143 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+defaultDA <- 180
+defaultRA <- 50
+defaultDB <- 365
+defaultRB <- 100
+defaultLogK <- -4.5
+
+
+hyperbolicDiscountFraction <- function(logk, delays){
+  return(1/(1+exp(logk)*delays))
+}
+
+ylimDemoPlot <- function(RA,RB){
+  if ( RA>0 & RB>0) {
+    ylim <- c(0,max(RA,RB))
+  } else if ( RA<0 & RB<0) {
+    ylim <- c(min(RA,RB), 0)
+  } else if ( RA<0 & RB>0) {
+    ylim <- c(RA,RB)
+  } else if ( RA>0 & RB<0) {
+    ylim <- c(RB,RA)
+  }
+  return(ylim)
+}
+
+max_delay <- 365+7
+
+
+ui <- fixedPage(
+  # TOP ROW WITH INPUTS
+  fixedRow(
+    column(width = 4,
+           h2("Hyperbolic Discounting"),
+           HTML("<p>Welcome to the discounting demo thing. Written by Benjamin T. Vincent of <a href='http://www.inferenceLab.com'>inferenceLab</a>.</p>"),
+           wellPanel(sliderInput(inputId = "logk",
+                       label = "discounting parameter, log(k)",
+                       min = -10,
+                       max = 1,
+                       value = defaultLogK,
+                       step = 0.01)
+                     )
+           ),
+    column(width = 4,
+           h4("Sooner reward"),
+           wellPanel(numericInput(inputId = "RA",
+                           label = "reward size (£)", 
+                           value = defaultRA),
+                     sliderInput(inputId = "DA",
+                          label = "delay (days)",
+                          min = 0,
+                          max = 365+7,
+                          value = defaultDA)
+                     )
+           ),
+    column(width = 4,
+           h4("Later reward"),
+           wellPanel(numericInput(inputId = "RB",
+                 label = "reward size (£)", 
+                 value = defaultRB),
+              sliderInput(inputId = "DB",
+                label = "delay (days)",
+                min = 0,
+                max = 365+7,
+                value = defaultDB)
+              )
+           ),
+  # NEW ROW SHOWING PLOTS
+  fixedRow(
+    column(width = 6,
+           h2("Discount function"),
+           p("The discount function is a proposed cognitive model. Personally, I see discount functions as descriptive cognitive models, not as truely explanatory."),
+           plotOutput("discountFunctionPlot")
+    ),
+    column(width = 6,
+           h2("Example decision"),
+           p("This plot shows the internal subjective value (y-axis) of the rewards (circles) as a function of time from now."),
+           plotOutput("examplePlot")
+    )
+  )
+)
+)
+
+
+
+
+
+server <- function(input, output){
+  # DISCOUNT FUNCTION
+  output$discountFunctionPlot <- renderPlot(
+    curve( hyperbolicDiscountFraction(input$logk, x),
+           from = 0,
+           to = max_delay,
+           n = 1001,
+           xlab = "Delay (days)",
+           ylab = "Discount fraction",
+           xlim = c(0,max_delay),
+           ylim = c(0,1)))
+  
+  # EXAMPLE DECISION PLOT
+  output$examplePlot <- renderPlot({
+    # delayed reward
+    curve( hyperbolicDiscountFraction(input$logk, input$DB-x) * input$RB,
+           from = 0,
+           to = input$DB,
+           n = 1001,
+           xlab = "Time from now (days)",
+           ylab = "subjective value",
+           xlim = c(0,max_delay),
+           ylim = ylimDemoPlot(input$RA,input$RB)
+    )
+    
+    # immediate reward
+    curve( hyperbolicDiscountFraction(input$logk, input$DA-x) * input$RA,
+           from = 0,
+           to = input$DA,
+           n = 1001,
+           add = TRUE)
+    lines(x=c(input$DA,input$DA),
+         y=c(0,input$RA))
+    
+    points(x=c(input$DA,input$DB),
+           y=c(input$RA,input$RB),
+           pch = 19,
+           cex = 1,
+           lwd = 2,
+           add = TRUE)
+    lines(x=c(input$DB,input$DB),
+          y=c(0,input$RB))
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
